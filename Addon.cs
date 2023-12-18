49eed8cfd6a0abbf0a209514d839ee46bdba6f5e
arkainas <kevin.vikander@gmail.com>
2023-12-18 14:53:36 +0100

    added command for create view but doesn't work tables now create with proper key references

---
+++ Addon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace Holidaymaker4;
+
+internal class Addon
+{
+    public async Task CreateView()
+    {
+        string dbUri = "Host=localhost;Port=5455;Username=postgres;Password=postgres;Database=idcgrupp4";
+
+        await using var db = NpgsqlDataSource.Create(dbUri);
+
+        await using (var cmd = db.CreateCommand(@"create view hotel_addons as 
+            select name, label, price from hotels_to_addons         
+            join hotels using (hotel_id)      
+            join addons using (addon_id);
+            select name, from hotel_addons where label = 'extra_bed'
+            intersect 
+            select name
+            from hotel_addons 
+            where label = 'all_inclusive';"))
+        {
+            await cmd.ExecuteReaderAsync();
+        }
+
+    }
+}
